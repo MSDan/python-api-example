04ae5f94c7c62fa156a6f12e4aef07f5a124bb8f
Sebastian Moldovan <sebastianmoldovan@Sebastians-MacBook-Pro.local>
2023-12-08 16:09:17 +0100

    Added csv lookup

--- main_template.py
+++ main_template.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from flasgger import Swagger
@@ -36,7 +37,59 @@ class UppercaseText(Resource):
 
         return jsonify({"text": text.upper()})
 
+
+class getIlsco(Resource):
+
+    def get(self):
+        """
+        This method responds to the GET request for this endpoint and returns a specific value from the CSV file.
+        ---
+        tags:
+        - CSV Processing
+        parameters:
+            - name: row
+              in: query
+              type: string
+              required: true
+              description: The row value to search in the CSV file
+            - name: column
+              in: query
+              type: string
+              required: true
+              description: The column value to search in the CSV file
+        responses:
+            200:
+                description: A successful GET request
+                content:
+                    application/json:
+                      schema:
+                        type: object
+                        properties:
+                            value:
+                                type: string
+                                description: The value in the specified row and column
+        """
+        # Replace 'your_file_path.csv' with the actual path to your CSV file
+        csv_file_path = 'ilsco_test.csv'
+        # Get values from query parameters
+        row = request.args.get('row')
+        col = request.args.get('column')
+        print(row,col)
+        
+        try:
+            # Read CSV file into a Pandas DataFrame
+            df = pd.read_csv(csv_file_path)
+            df.set_index('LugSize', inplace=True)
+            # Find the value at the specified row and column
+            value = df.loc[row, col]
+            
+            return {"value": value}, 200
+        except Exception as e:
+            return {"error": str(e)}, 400
+
+
 api.add_resource(UppercaseText, "/uppercase")
+api.add_resource(getIlsco, "/get_csv_value")
 
 if __name__ == "__main__":
     app.run(debug=True)
